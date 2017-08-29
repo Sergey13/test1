798d0376c89a1829428dc637f01b59fbc43c219f
Sergey Guk <sv-garant@mail.ru>
2017-08-29 15:42:30 +0300

    2 izmemenie

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>test</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 	<h1>hello world</h1>
 	<h2>hello world2</h2>
+	<h2>hello world2</h2>
+	<p>ggggggggggggggg</p>
 </body>
 </html>
